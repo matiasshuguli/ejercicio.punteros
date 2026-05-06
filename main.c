379f5b4dee822a8ac669a6ea4aa3954554af9769
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-06 15:00:24 -0500

    Tercer commit agregar idireccion

--- main.c
+++ main.c
@@ -4,16 +4,15 @@ int main (int argc, char *argv[]) {
 
     int num = 150, a;
     int *x; // puntero x
-
     x = &num; // almacena la direcion de num
+    a = *x;
     
-    printf("%d %p",num,x); // imprime 
-                           // %p  te da la direccion de la variable
-                           // %d  te da el valor de la variable num
-
+    printf("%d %d %p",num,a,x); // imprime, %p  te da la direccion de la variable,  %d te da el valor de la variable num
+                
     float pi = 3.1415, d;
-    float *ptrF; 
-    ptrF = &pi;
+    float *ptrF; // definicion de puntero
+    ptrF = &pi;  // puntero almacena la direccion de pi
     printf("%f %p",pi,ptrF); // imprime 
+
     return 0;
 }
