20b7f473b57feea8de0a5c3bd4517a3093c9d00f
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-06 14:42:48 -0500

    Cambio de int a float usando punteros

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-    int num = 150, a;
-    int *x; // puntero
+    float num = 150.5;
+    float *x; // puntero a float
 
-    x = &num; // almacena la direcion de num
-    
-    printf("%d %p",num,x); // imprime 
-                           // %p  te da la direccion de la variable
-                           // %d  te da el valor de num
-    return 0
+    x = &num; // dirección de num
+
+    printf("%f %p", num, x); // %f para float, %p para dirección
+
+    return 0;
+}
