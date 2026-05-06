cc884499a12dbbbe8ebaacb2d291abc1b0a7b484
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-06 15:10:15 -0500

    Cuarto commit, agregamos caracteres

--- main.c
+++ main.c
@@ -7,12 +7,16 @@ int main (int argc, char *argv[]) {
     x = &num; // almacena la direcion de num
     a = *x;
     
-    printf("%d %d %p",num,a,x); // imprime, %p  te da la direccion de la variable,  %d te da el valor de la variable num
+    printf("%d %d %p",num,a,x); // imprime, %p te da la direccion de la variable, %d te da el valor de la variable num
                 
     float pi = 3.1415, d;
     float *ptrF; // definicion de puntero
     ptrF = &pi;  // puntero almacena la direccion de pi
-    printf("%f %p",pi,ptrF); // imprime 
 
+    printf("%.3f %.3f %p",pi, d,ptrF); // imprime 
+
+    char l = 'a', c; 
+    char *ptrC; // definicion del puntero 
+    printf("%c %c  %p",l,c, ptrC);
     return 0;
 }
